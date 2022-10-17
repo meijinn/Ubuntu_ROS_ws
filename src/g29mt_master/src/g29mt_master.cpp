a113fd853bcbefe21cc0e78f9e42d04904f70ed0
meijinn <takahashishotaro21@gmail.com>
2022-10-17 23:53:18 +0900

    g29のセッティングを開始する

---
+++ src/g29mt_master/src/g29mt_master.cpp
@@ -0,0 +1,48 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Joy.h>  
+#include <std_msgs/UInt8MultiArray.h>
+
+std_msgs::UInt8MultiArray controller;
+
+void joy_callback(const sensor_msgs::Joy &joy_msg)
+{
+  // 処理内容を記述 踏むと1になるからps3に-1をかける
+  // joy_msg.axes[5]=gas, joy_msg.axes[2]=brake, joy_msg.axis[0]=steering
+  controller.data[0] = ((joy_msg.axes[0]*(-1)*180)+180)/2;
+  //float gas = joy_msg.axes[2]*(-1)+93;//low
+  //float gas = (joy_msg.axes[2]*-3+183)/2;//second
+  //float gas = joy_msg.axes[2]*-3+92;//third
+  //float gas = (joy_msg.axes[2]*-7+183)/2;//top
+  float gas = (joy_msg.axes[2]*-11+183)/2;//overtop
+  //float brake = (joy_msg.axes[3]*21*-1+207)/2; //default
+  //float brake = joy_msg.axes[3]*-10+103;//low
+  //float brake = joy_msg.axes[3]*10+103;//second
+  //float brake = (joy_msg.axes[3]*21+203)/2;//third
+  //float brake = joy_msg.axes[3]*11+102;//top
+  float brake = joy_msg.axes[3]*(11)+101;//overtop
+
+  int throttle = int((gas+brake)/2);
+  controller.data[1] = throttle;
+  //ROS_INFO("steering:%d",steering);   // スティック0の状態を表示 (-1 ～ 1)
+  ROS_INFO("steering:%d",controller.data[0]);  // ボタン0の状態を表示 (0 or 1)
+  ROS_INFO("throttle:%d",controller.data[1]);
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "joy_pub_node");
+  ros::NodeHandle nh;
+  ros::Subscriber sub = nh.subscribe("joy", 10, joy_callback);
+
+  ros::Publisher controller_pub = nh.advertise<std_msgs::UInt8MultiArray>("controller", 1);
+  ros::Rate loop_rate(100);
+
+  while (ros::ok())
+  {
+    controller.data.resize(2);
+    controller_pub.publish(controller);
+    ros::spinOnce();
+    loop_rate.sleep();
+  }
+  return 0;
+}
